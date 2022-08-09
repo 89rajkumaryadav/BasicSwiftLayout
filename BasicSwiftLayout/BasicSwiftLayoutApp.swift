0263a6cc9cb3494997e871170954d9a7626aecac
rajkumaryadav89 <rajkumar.yadav@ecolane.com>
2022-08-09 12:01:40 +0530

    Initial Commit

---
+++ BasicSwiftLayout/BasicSwiftLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftLayoutApp.swift
+//  BasicSwiftLayout
+//
+//  Created by Rajkumar Yadav on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicSwiftLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicSwiftLayout
+//
+//  Created by Rajkumar Yadav on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
